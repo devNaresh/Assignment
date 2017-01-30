a56cb0f8e8414fe6e16e921fd716648393321ab1
Naresh <lnaresh60@gmail.com>
2017-01-30 20:07:18 +0530

    added index.html

--- uber_assignment_SF_movies/uber_assignment_SF_movies/settings.py
+++ uber_assignment_SF_movies/uber_assignment_SF_movies/settings.py
@@ -133,4 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
